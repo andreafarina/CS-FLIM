e3e8c5c4fa5089530423f50a5e76c26866b58ac9
Andrea Farina <andrea.farina@polimi.it>
2021-05-11 12:58:25 +0200

    Added some features at CSPattern: background, CWshifted modality

--- Patterns/CSpattern.m
+++ Patterns/CSpattern.m
@@ -1,21 +1,24 @@
 classdef CSpattern
-    %PATTTERN this class creates/allocate/save 2D
-    % squared patterns
+    %CSPATTERN this class creates/allocate/save 2D
+    % patterns
     %   Detailed explanation goes here
     
     properties
         type
         order
         Npatt
-        posneg = false;
+        kind        % posneg % shifted % shiftedCW
+        cw
         dim
-        stack
-        Mmatrix
-        % random seed
-        seed = 122;
-        % scrambled dedicated
+        stack       % image stacks
+        Tmatrix     % matrix of true patterns
+        Mmatrix     % measurement matrix
+        seed = 122; % random seed
+        % scrambledH dedicated
         Pr
         Pc
+        % shiftedCW dedicated
+        indexCW
     end
     
     methods
@@ -30,9 +33,11 @@ classdef CSpattern
                 case 'hadamard'
                     obj.dim = [order,order];
                     obj.Mmatrix = hadamard(order.^2);
-                    obj.stack = reshape(obj.Mmatrix,[order,order,order^2]);
+                    obj.Tmatrix = obj.Mmatrix;
+                    obj.stack = reshape(obj.Tmatrix,[order,order,order^2]);
                     obj.order = order;
-                
+                    obj.cw = 1;
+                    
                 case 'wh'
                     for i = 1:order^2
                         d = zeros(order,order);
@@ -40,11 +45,13 @@ classdef CSpattern
                         hk = fwht2D(d,order);
                         obj.stack(:,:,i) = hk;
                         obj.Mmatrix(i,:) = hk(:);
+                        obj.Tmatrix = obj.Mmatrix;
                     end
                     obj.dim = [order,order];
                     obj.order = order;
-                
-                case 'scrambled'
+                    obj.cw = 1;
+                    
+                case 'scrambledh'
                     obj.dim = [order,order];
                     K = order^2;
                     H = hadamard(K);
@@ -52,20 +59,34 @@ classdef CSpattern
                     obj.Pr = I(randperm(K),:);
                     obj.Pc = I(randperm(K),:);
                     obj.Mmatrix = obj.Pr * H * obj.Pc;
-                    obj.stack = reshape(obj.Mmatrix,[order,order,order^2]);
+                    obj.Tmatrix = obj.Mmatrix;
+                    obj.stack = reshape(obj.Tmatrix,[order,order,order^2]);
                     obj.order = order;
+                    obj.cw = 1;
                     
                 case 'bernoulli'
                     obj.dim = [order,order];
                     obj.Mmatrix = random('bino',1,1/2,[order^2,order^2])*2 - 1;
+                    obj.Tmatrix = obj.Mmatrix;
                     obj.stack = reshape(obj.Mmatrix,[order,order,order^2]);
                     obj.order = order;
-                
+                    obj.cw = 0;
+                    
                 case 'fourier'
                     % to be implemented
+                otherwise
+                    disp('Pattern not found');
+                    return;
             end
             obj.Npatt = size(obj.Mmatrix,1);
-            obj.type = type;
+            obj.type = lower(type);
+        end
+        
+        function obj = restore(obj)
+            obj.Mmatrix = obj.Tmatrix;
+            obj.stack = reshape(obj.Tmatrix,[obj.order,obj.order,obj.order^2]);
+            obj.dim = [obj.order,obj.order];
+            obj.Npatt = size(obj.Tmatrix,1);
         end
         
         function obj = resample(obj,dim)
@@ -78,26 +99,81 @@ classdef CSpattern
             obj.dim = dim;
         end
         
-        function obj = TrueToPosNeg(obj)
+        function obj = ConvertToPosNeg(obj)
+            if obj.CheckKind
+                return;
+            end
             nrow = obj.Npatt;
             Mpos = obj.Mmatrix;
             Mpos(Mpos<0) = 0;
             Mneg = -obj.Mmatrix;
             Mneg(Mneg<0) = 0;
             M = [Mpos;Mneg];
-            % permutatio matrix
+            % permutation matrix
             Prr = zeros(2*nrow);
             Prr(1:nrow,1:2:end) = eye(nrow);
             Prr((1:nrow)+nrow,2:2:end) = eye(nrow);
             obj.Mmatrix = Prr' * M;
             obj.stack = reshape(obj.Mmatrix',[obj.dim(1),obj.dim(2),2*obj.Npatt]);
             obj.Npatt = 2 * obj.Npatt;
-            obj.posneg = true;
+            obj.kind = 'posneg';
         end
         
-        function obj = TrueToShifted(obj)
+        function obj = ConvertToShifted(obj)
+            if obj.CheckKind
+                return;
+            end
             obj.Mmatrix = obj.Mmatrix + abs(min(obj.Mmatrix,[],2));
             obj.Mmatrix = obj.Mmatrix./max(obj.Mmatrix,[],2);
+            obj.stack = reshape(obj.Mmatrix',[obj.dim(1),obj.dim(2),obj.Npatt]);
+            obj.kind = 'shifted';
+        end
+        
+        function obj = DeleteCW(obj)
+            if obj.cw == 0
+                disp('this pattern does not have a CW');
+            end
+            a = sum(obj.Tmatrix,2);
+            [~,imax] = max(a);
+            obj.Mmatrix(imax,:) = [];
+            obj.stack(:,:,imax) = [];
+            obj.Npatt = obj.Npatt - 1;
+            obj.cw = 0;
+        end
+        
+        function obj = ConvertToCWShifted(obj,num)
+            %ADDPOSNEGCW add posneg patterns evenly separated in the stack
+            % only for hadamard, wh, scrambled
+            if strcmpi(obj.type,'fourier')
+                disp('This does not work for Fourier patterns');
+            end
+            if obj.cw == 1
+                disp('Please Delete the CW!');
+                return;
+            end
+            if ~strcmpi(obj.kind,'posneg')
+                disp('Please convert to posneg!');
+                return;
+            end
+            obj.indexCW = zeros(1,obj.Npatt);
+            obj.indexCW(1:2:end) = 1;
+            if num == 1
+                obj.indexCW(2) = 1;
+            else
+                [~,d] = find((obj.indexCW == 0));
+                b = round(linspace(1,numel(d),num));
+                obj.indexCW(d(b)) = 1;
+            end
+            obj.Npatt = sum(obj.indexCW);
+            obj.Mmatrix(~obj.indexCW,:) = [];
+            obj.stack(:,:,~obj.indexCW) = [];
+            obj.kind = 'shiftedCW';
+        end
+        
+        function obj = AddZeroPattern(obj)
+            d = zeros(1,size(obj.Mmatrix,2));
+            obj.Mmatrix = [d;obj.Mmatrix];
+            obj.Npatt = obj.Npatt + 1;
             obj.stack = reshape(obj.Mmatrix',[obj.dim(1),obj.dim(2),obj.Npatt]);
         end
         
@@ -110,15 +186,15 @@ classdef CSpattern
             Nx = size(obj.stack,1);
             Ny = size(obj.stack,2);
             stride = Nx + space;
-            A = mean(obj.stack(:))*ones(stride*round(sqrt(obj.Npatt)),stride*round(sqrt(obj.Npatt)));
-            figure;ax = gca;
+            A = mean(obj.stack(:))*ones(stride*ceil(sqrt(obj.Npatt)),stride*ceil(sqrt(obj.Npatt)));
+            figure;%ax = gca;
             for ip = 1:obj.Npatt
                 d = zeros(stride,stride);
                 d(space + 1:stride,1:Ny) = obj.stack(:,:,ip);
-                id = zeros(round(sqrt(obj.Npatt)));
+                id = zeros(ceil(sqrt(obj.Npatt)));
                 id(ip) = 1;
                 A = A + kron(id',d);
-                imagesc(ax,A);
+                imagesc(A)
             end
         end
         
@@ -161,5 +237,15 @@ classdef CSpattern
             end
         end
     end
-end
     
+    methods (Access = protected)
+        function setYN = CheckKind(obj)
+            if ~isempty(obj.kind)
+                disp(['Kind already set to ',obj.kind]);
+                setYN = 1;
+            else
+                setYN = 0;
+            end
+        end
+    end
+end
