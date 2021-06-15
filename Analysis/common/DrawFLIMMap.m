6880aeccde727d89a71fc0110c6b1a0016aab278
Alberto <alberto.ghezzi@polimi.it>
2021-06-15 17:23:11 +0200

    Added function to create FLIM maps

---
+++ Analysis/common/DrawFLIMMap.m
@@ -0,0 +1,49 @@
+function [] = DrawFLIMMap(lifetime,concentration,N,residuals)
+
+% Plot the results
+for li = 1:size(lifetime,1)
+    for n = 1:N
+        figure(li), subplot(1,N+1,n),
+        % Colormap is from 0 to 1. To avoid 1s and 0s to be both
+        % colored in red,  The limit
+        % is 0°,260°. Then I set the 0 to the min lifetime
+        A = squeeze(lifetime(li,:,:,n));
+        B = A-min(min(A(A>0)));
+        B(B<0) = 0;
+        I(:,:,1) = B./max(max(B));
+        % Additionally, I want shot times in blue and long in
+        % red:
+        I(:,:,1) =  1-I(:,:,1);
+        %I limit the color wheel to a degree
+        % lower than 360° (both 0° and 360° are red).
+        I(:,:,1) = I(:,:,1).*(260/360);
+        I(:,:,2) = ones([size(I,1) size(I,2)]);
+        I(:,:,3) = squeeze(concentration(li,:,:,n));
+        
+        imagesc(hsv2rgb(I)),axis image,
+        hsv = [flipud(linspace(0,260/360,255)); ones(1,255);ones(1,255)]';
+        rgb = hsv2rgb(hsv);
+        colormap(flipud(rgb))
+        cb = colorbar;
+        cb.Ticks = linspace(0,1,8);
+        cb.TickLabels = round(linspace(min(min(A(A>0))),max(max(A)),length(cb.Ticks)),1);
+        title(['Composite lifetime map for \tau_',num2str(n)])
+    end
+    if nargin == 4
+    resplot = subplot(1,N+1,N+1);
+    imagesc(squeeze(residuals(li,:,:)));axis image,colormap(resplot,'parula'),colorbar
+    title('Residuals')
+    end
+end
+
+
+for li=1:size(lifetime,1)
+    figure(size(lifetime,1)+li)
+    for n = 1:N
+        subplot(1,N,n)
+        taus = squeeze(lifetime(li,:,:,n));
+        histogram(taus(taus>0),round(numel(lifetime(li,:,:,n))/30))
+    end
+end
+
+end
