8d334c91176c8865526b543a1ff57dd792d68aea
Alberto <alberto.ghezzi@polimi.it>
2021-11-12 01:05:33 +0100

    Changed the function for the reconstruction gaining a factor of 80 in the required time

--- Analysis/common/SHReconstruction.m
+++ Analysis/common/SHReconstruction.m
@@ -1,11 +1,14 @@
-function [im] = SHReconstruction(had,Pc,Pr,flag_flip)
+function [im] = SHReconstruction(had,M,flag_flip)
 % performs a ScrambledHadamard inversion on a had(time,lambda,coeff) dataset.
 % flag_flip is a tick that flips left-right the reconstructed image
 L = size(had,2);
 T = size(had,1);
+M = squeeze(M(1:2:end,:,:)-M(2:2:end,:,:));
+%B = fwht(eye(sqrt(size(had,3))^2),[],'hadamard');
 for li = 1:L
     parfor ti = 1:T
-        x = Pc'*fwht(eye(sqrt(size(had,3))^2),[],'hadamard')*Pr'*squeeze(had(ti,li,:));
+        % x = Pc'*B*Pr'*squeeze(had(ti,li,:));
+        [x,flag] = lsqr(M,squeeze(had(ti,li,:)));
         A = reshape(x,[sqrt(size(had,3)) sqrt(size(had,3))]);
         if flag_flip == 1
             im(ti,li,:,:) = fliplr(A);
@@ -16,3 +19,4 @@ for li = 1:L
     disp(['Progress...',num2str(round(100*li/L)),'%'])
 end
 end
+
