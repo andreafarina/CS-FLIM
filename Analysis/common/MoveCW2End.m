59acd1fda3f06f597b030e9ddcfa2cbd169f16ee
Alberto <alberto.ghezzi@polimi.it>
2022-07-25 17:40:38 +0200

    For best results, CW is actually moved to beginning

--- Analysis/common/MoveCW2End.m
+++ Analysis/common/MoveCW2End.m
@@ -1,10 +1,9 @@
-function [had,M] = MoveCW2End(had,M)
+function [had,M] = MoveCW2End(had,M,q)
 % We saw that reconstruction with the CW pattern affects the PSNR: there is
 % a step in PSNR if, by tuning the CR, the CW pattern is included or not in
 % the had vector.
 
-[~,q] = max(had);
-M = M([1:(q(1)-1) (q(1)+1):size(M,1) q(1)],:); 
-had = had([1:(q(1)-1) (q(1)+1):length(had) q(1)],:);
+M = M([q(1) 1:(q(1)-1) (q(1)+1):size(M,1)],:); 
+had = had([q(1) 1:(q(1)-1) (q(1)+1):length(had)],:);
 
 end
