91b8ef7b09e8602d86e1d1165f239d8b54bc405c
Alberto <alberto.ghezzi@polimi.it>
2022-07-25 17:39:42 +0200

    Autoclose figure after execution

--- Analysis/common/InvertwTVAL3.m
+++ Analysis/common/InvertwTVAL3.m
@@ -17,21 +17,21 @@ opts.beta = beta; %default value, suggested by authors
 opts.maxit = 3000;
 opts.tol = 1E-8;
 opts.isreal = true;
-
 %% Display
 if PLOT_ITERATIONS == 1
     [~, pPix] = min(abs(imA(:)-0.5*max(imA(:))));
     [rPix,cPix] = ind2sub([K K],pPix);
     normalize = @(X)X./max((X));
-    figure,
+    c = figure;
     subplot(1,3,1), h1 = imagesc(imA); axis image
-    subplot(1,3,2), h2 = plot(squeeze(sum(had,2:3))); axis square
-    subplot(1,3,3), h3 = plot(squeeze(sum(had,[1 3]))); axis square
+    subplot(1,3,2), h2 = plot(squeeze(sum(had,[1 3]))); axis square
+    subplot(1,3,3), h3 = plot(squeeze(sum(had,2:3))); axis square
 end
 %% Solve
+[~,b] = max(squeeze(sum(had,1:2)));
 for ti = 1:size(had,1)
     parfor li = 1:size(had,2)
-        [had_rec, M1] = MoveCW2End(squeeze(had(ti,li,:)),M);  
+        [had_rec, M1] = MoveCW2End(squeeze(had(ti,li,:)),M,b);  
         [U, ~] = TVAL3(M1(1:m,:),had_rec(1:m),K,K,opts);
         if sum(isnan(U(:)))>0
         res(ti,li,:,:) = zeros(K,K);
@@ -48,4 +48,5 @@ for ti = 1:size(had,1)
     disp(['Processed: ',num2str(100*ti/size(had,1)),'%']);
 end
 im_TVAL = res;
+close(c);
 end
