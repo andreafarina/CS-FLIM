8e112e8e7a4ccaa4c62515237c749aaa8612002d
Alberto <alberto.ghezzi@polimi.it>
2023-03-28 17:27:02 +0200

    File creation to draw a colored envelop in multiple graphs

---
+++ Analysis/common/plot_coloredenvelope.m
@@ -0,0 +1,22 @@
+function [patch,h2,h] = plot_coloredenvelope(x,data, ref)
+
+        options.handle     = figure;
+        options.color_area = [128 193 219]./255;   
+        options.color_line = [ 52 148 186]./255;
+        options.alpha      = 0.5;
+        options.line_width = 1;
+
+    options.x_axis = x;
+     
+    % Plotting the result
+    h = figure(options.handle);
+    x_vector = [options.x_axis, fliplr(options.x_axis)];
+    patch = fill(x_vector, [data+ref,fliplr(ref-data)], options.color_area);
+    set(patch, 'edgecolor', 'none');
+    set(patch, 'FaceAlpha', options.alpha);
+    hold on;
+    h2 = plot(options.x_axis, ref, 'color', options.color_line, ...
+        'LineWidth', options.line_width);
+    hold off;
+    
+end
