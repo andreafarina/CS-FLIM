d760d62bd203325521af1946cb3ef4480aeeb4ed
Alberto <alberto.ghezzi@polimi.it>
2023-03-28 17:42:19 +0200

    Changed imresize method from nearest to box

--- Analysis/data_fusion/routines/spaceResample.m
+++ Analysis/data_fusion/routines/spaceResample.m
@@ -8,7 +8,7 @@ hypcube = zeros(size(input,1),size(input,2),newSize,newSize);
 %downsample process
 for i=1:size(hypcube,1)
     for j=1:size(hypcube,2)
-        temp = imresize(squeeze(input(i,j,:,:)),[newSize newSize],'nearest'); %resize
+        temp = imresize(squeeze(input(i,j,:,:)),[newSize newSize],'box'); %resize
         temp2 = norm(tens2vec(input(i,j,:,:))); %old norm
         temp3 = temp2*temp/norm(temp(:)); %make new norm = old norm
         hypcube(i,j,:,:) = temp3;
