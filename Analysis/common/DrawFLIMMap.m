226c4dd0e8f941f486a1fb31f061c5db5000057b
Alberto <alberto.ghezzi@polimi.it>
2024-05-05 17:08:01 +0200

    Add feature to handle subplots and lower upper bounds

--- Analysis/common/DrawFLIMMap.m
+++ Analysis/common/DrawFLIMMap.m
@@ -1,13 +1,29 @@
-function [h] = DrawFLIMMap(lifetime,concentration,N,residuals)
+function [] = DrawFLIMMap(lifetime,concentration,N,residuals,lb,ub,ax)
 
+if nargin > 4
+    for i = 1:size(lifetime,1)
+    lifetime(lifetime(:)>ub)=0;
+    lifetime(lifetime(:)<lb)=0;
+    lifetime(1,1,32) = ub;
+    lifetime(1,1,31) = lb;
+    lifetimeFast(1,1,32) = ub;
+    lifetimeFast(1,1,31) = lb;
+    end
+end
 % Plot the results
 for li = 1:size(lifetime,1)
     for n = 1:N
-        h = figure(li+50); subplot(1,N+1,n),
+        if nargin < 5
+            h = figure(li+50); subplot(1,N+1,n)
+        end
         % Colormap is from 0 to 1. To avoid 1s and 0s to be both
         % colored in red,  The limit
         % is 0°,260°. Then I set the 0 to the min lifetime
-        A = squeeze(lifetime(li,:,:,n));
+        if length(size(lifetime)) == 4
+            A = squeeze(lifetime(li,:,:,n));
+        else
+            A = squeeze(lifetime(li,:,:));
+        end
         B = A-min(min(A(A>0)));
         B(B<0) = 0;
         I(:,:,1) = B./max(max(B));
@@ -23,23 +39,28 @@ for li = 1:size(lifetime,1)
         imagesc((hsv2rgb(I))),axis image,
         hsv = [flipud(linspace(0,260/360,255)); ones(1,255);ones(1,255)]';
         rgb = hsv2rgb(hsv);
-        %colormap(flipud(rgb))
-        colormap(rgb)
+
+        if nargin==7
+            colormap(ax,rgb) %if comparison with camera
+        else
+            colormap(rgb)
+        end
         cb = colorbar;
         cb.Label.String = 'Lifetime [ns]';
         cb.Ticks = linspace(0,1,8);
         cb.TickLabels = round(linspace(min(min(A(A>0))),max(max(A)),length(cb.Ticks)),1);
         %title(['Composite lifetime map for \tau_',num2str(n)])
     end
+
     if nargin == 4
-    resplot = subplot(1,N+1,N+1);
-    Z = squeeze(residuals(li,:,:));
-    h = imagesc(Z);axis image,colormap(resplot,'parula'),colorbar,caxis([0 1])
-    % Set nan values to transparent: 
-    set(h,'alphadata',Z>0)
-    % Make the background black: 
-    set(gca,'color','black')
-    title('Residuals')
+        resplot = subplot(1,N+1,N+1);
+        Z = squeeze(residuals(li,:,:));
+        h = imagesc(Z);axis image,colormap(resplot,'parula'),colorbar,caxis([0 1])
+        % Set nan values to transparent: 
+        set(h,'alphadata',Z>0)
+        % Make the background black: 
+        set(gca,'color','black')
+        title('Residuals')
     end
 end
 
